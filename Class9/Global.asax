390447d8ff9a4d7b338be6aa52c2e7067142120e
KarenSpringford <jkspringford@gmail.com>
2016-07-21 09:55:47 -0400

    Add project files.

---
+++ Class9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Class9.MvcApplication" Language="C#" %>
---
+++ Class9/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Class9.Startup))]
+namespace Class9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
